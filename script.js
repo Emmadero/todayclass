a97e8e44415480b0f93ebe89c18da2a5a705db0e
Judechukwuma14 <ebukajude14@gmail.com>
2026-09-02 18:22:36 +0300

    first push for today

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+// key variableName = value
+
+// alert("Welcome to Javascript")
+// prompt("What is your name?")
+// prompt("How old are you?")
+// confirm("Are you sure you want to continue?")
+
+// console.log("This is a log");
+// console.error("This is an error");
+// console.warn("This is a warning");
+// console.info("This is an info");
+
+
+// let const
+
+let userName = "Mama"
+console.log(userName)
+
+const user_name = "Ujunwa"
+console.log(user_name)
+
+userName = "Victor"
+console.log(userName)
+
+
+
+
+
